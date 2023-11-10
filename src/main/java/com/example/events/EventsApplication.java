35dc856f384d3fa06686009062f4347130f20789
Mauricio Roa <mauricio.roa@outlook.com>
2023-11-09 23:43:35 -0500

    Ajuste para no tomar el .env

--- src/main/java/com/example/events/EventsApplication.java
+++ src/main/java/com/example/events/EventsApplication.java
@@ -2,12 +2,10 @@ package com.example.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
-@PropertySources({
-	@PropertySource("file:.env")
-})
+//@PropertySources({
+//	@PropertySource("file:.env")
+//})
 @SpringBootApplication
 public class EventsApplication {
 
